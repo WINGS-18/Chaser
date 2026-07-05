fef1abc6e2162a6912ecc4d03f0a264318069290
Wings <kelaskarmanoj18@gmail.com>
2026-07-05 21:45:15 +0530

    removed duplicated headers and removed trail code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-#include <array>
-#include "player.h"
-#include "enemy.h"
-#include "screen.h"
+#include "game_loop.h"
 
 int main() {
-    Chaser::Enemy e({5, 6}, 3, 'E');
-    Chaser::Player p({6, 5}, 1, 'U');
-    Chaser::Screen s;
-    //s.screenReset();
-    s.insertEntity(e);
-    s.insertEntity(p);
-    s.printScreen();
     
+    //gl.sleep(1000);
+    Chaser::GameLoop gl;
+    gl.startGame();
+
     return 0;
 }
