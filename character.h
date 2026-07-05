d0e347db6523de969ee8b9400e580a01e98e0400
Wings <kelaskarmanoj18@gmail.com>
2026-07-05 21:43:44 +0530

    updated win condition and threshold values. Also fixed issue where p_getY() returned x

--- character.h
+++ character.h
@@ -3,8 +3,8 @@
 
 namespace Chaser {
 
-    inline const Vector2C g_winCon{49, 19};     //win condition
-    inline const Vector2C treshold{35, 19};     //point after which chaser gets speed boost
+    inline const Vector2C g_winCon{49, 9};     //win condition
+    inline const Vector2C treshold{35, 0};     //point after which chaser gets speed boost
     inline const int g_minInvSpeed = 0;         //minimum speed a character can attain
     inline const int g_maxInvSpeed = 3;         //maximum speed a character can attain
 
@@ -22,7 +22,7 @@ namespace Chaser {
 
         int p_getX() const {return p_coord.v_x;}        //returns x
 
-        int p_getY() const {return p_coord.v_x;}        //returns y
+        int p_getY() const {return p_coord.v_y;}        //returns y
 
         int p_getSpeed() const {return p_invSpeed;}     //returns speed
 
