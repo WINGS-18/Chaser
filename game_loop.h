57c73cbc18f93477ce9c8ccc80c9dae16f75b32c
Wings <kelaskarmanoj18@gmail.com>
2026-07-06 18:54:13 +0530

    added forward declarations and comments

--- game_loop.h
+++ game_loop.h
@@ -2,20 +2,20 @@
 
 namespace Chaser {
 
+    /*forward declaration: A promise made to the compiler
+    that such classes are there ahead*/
+    class Enemy;
+    class Player;
+
     class GameLoop {
     public:
         GameLoop();     //constructor declaration
         
         void sleep(int time);       //pauses the program for given time
-        const char keyGiver();        //returns the pressed key
-        bool gameEnd(char key);
-        int startGame(); //{
-            // int player_speed = 0;
-            // int enemy_speed = 0;
-            // while(true) {
-
-            // }
-        //}
+        const char c_keyGiver();        //returns the pressed key
+        bool gameEnd(char key);     //did player intentionally end the game
+        bool resultDisplay(const Enemy& e, const Player& p);        //displays the result
+        int startGame();        //main game loop
     };
 
 }
